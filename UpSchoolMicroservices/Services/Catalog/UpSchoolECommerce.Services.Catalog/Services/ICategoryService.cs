326181550f160459fc1d47753c6dd997900382d9
Serap BERAN <79447748+serapberan@users.noreply.github.com>
2023-01-17 20:56:04 +0300

    microservice_1501

---
+++ UpSchoolMicroservices/Services/Catalog/UpSchoolECommerce.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UpSchoolECommerce.Services.Catalog.Dtos;
+using UpSchoolECommerce.Shared.Dtos;
+
+namespace UpSchoolECommerce.Services.Catalog.Services
+{
+    public interface ICategoryService
+    {
+        Task<ResponseDto<List<CategoryDto>>> GetAllAsync();
+        Task<ResponseDto<CategoryDto>> CreateAsync(CategoryDto categoryDto);
+        Task<ResponseDto<CategoryDto>> GetByIdAsync(string id);
+
+    }
+}
